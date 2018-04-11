9717a0b9559e747052bf4411fbea6102f5aa8f0c
rmbw74 <rmbw74@gmail.com>
2018-04-10 21:47:42 -0500

    added sounds

---
+++ Asteroids/SpaceShooter/Assets/Scripts/DestroyByBoundry.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundry : MonoBehaviour 
+{
+	void OnTriggerExit(Collider other)
+	{
+		//destroy everything that leaves the trigger
+		Destroy(other.gameObject);
+	}
+}
---
+++ Asteroids/SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundry
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody rb;
+	private AudioSource audiosource;
+	public float speed;
+	public float tilt;
+	public Boundry boundry;
+
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		audiosource = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{	
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+		    Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			audiosource.Play ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 
+			(
+				Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax),
+				0.0f,
+				Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax)
+			);
+		rb.rotation = Quaternion.Euler(0.0f,0.0f,rb.velocity.x * -tilt);
+	}
+
+}
